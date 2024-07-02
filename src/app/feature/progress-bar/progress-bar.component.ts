04a6f70bedfcec6ea4c30d904fcf736a4e319d63
Fahimeh Ahmadi <fahimehahmadi.fafa@gmail.com>
2024-07-02 14:43:27 +0330

    add router Link Active in tabs

--- src/app/feature/progress-bar/progress-bar.component.ts
+++ src/app/feature/progress-bar/progress-bar.component.ts
@@ -1,31 +1,18 @@
 import {Component, OnInit} from '@angular/core';
-import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
   styleUrl: './progress-bar.component.css'
 })
-export class ProgressBarComponent implements OnInit {
+export class ProgressBarComponent {
   steps = [
-    { label: 'اطلاعات فردی', route: '/personal-info' },
-    { label: 'آپلود مدارک', route: '/upload-documents' },
-    { label: 'اطلاعات تماس', route: '/contact-info' },
-    { label: 'مرور اطلاعات', route: '/review-info' }
+    {label: 'اطلاعات فردی', route: '/personal-info'},
+    {label: 'آپلود مدارک', route: '/upload-documents'},
+    {label: 'اطلاعات تماس', route: '/contact-info'},
+    {label: 'مرور اطلاعات', route: '/review-info'}
   ];
-  currentRoute: string = '';
 
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.currentRoute = event.urlAfterRedirects;
-      }
-    });
-  }
-
-  isActive(route: string): boolean {
-    return this.currentRoute === route;
+  constructor() {
   }
 }
