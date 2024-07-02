2360e9c545e28b4727d071327f2dc6dd9bc8fdb2
Fahimeh Ahmadi <fahimehahmadi.fafa@gmail.com>
2024-07-02 17:26:43 +0330

    add contact Info

--- src/app/feature/contact-info/contact-info.component.ts
+++ src/app/feature/contact-info/contact-info.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {UserDataService} from "../../core/services/user-data/user-data.service";
+import {ContactInfo, User} from "../../core/models/user-data.model";
 
 @Component({
   selector: 'app-contact-info',
@@ -9,7 +10,10 @@ import {UserDataService} from "../../core/services/user-data/user-data.service";
   styleUrl: './contact-info.component.css'
 })
 export class ContactInfoComponent implements OnInit {
-  contactInfoForm !: FormGroup;
+  contactInfoFG !: FormGroup;
+  provinceFC!: FormControl;
+  cityFC!: FormControl;
+  addressFC!: FormControl;
 
   constructor(private fb: FormBuilder,
               private userDataService: UserDataService,
@@ -18,17 +22,34 @@ export class ContactInfoComponent implements OnInit {
 
   ngOnInit() {
     const userData = this.userDataService.getUserData();
-    this.contactInfoForm = this.fb.group({
-      province: [userData.contactInfo.province || '', Validators.required],
-      city: [userData.contactInfo.city || '', Validators.required],
-      address: [userData.contactInfo.address || '', Validators.required]
+    this.setInitialData(userData);
+  }
+
+  setInitialData(userData: User): void {
+    this.provinceFC = this.fb.control(userData.contactInfo.province || '', Validators.required);
+    this.cityFC = this.fb.control(userData.contactInfo.city || '', Validators.required);
+    this.addressFC = this.fb.control(userData.contactInfo.address || '', Validators.required);
+
+    this.contactInfoFG = this.fb.group({
+      province: this.provinceFC,
+      city: this.cityFC,
+      address: this.addressFC,
     });
   }
 
   onSubmit() {
-    if (this.contactInfoForm.valid) {
-      this.userDataService.setUserData(this.contactInfoForm.value);
+    if (this.contactInfoFG.valid) {
+      let contactInfo: ContactInfo = {
+        province: this.provinceFC.value,
+        city: this.cityFC.value,
+        address: this.addressFC.value,
+      }
+      this.userDataService.setUserData({contactInfo});
       this.router.navigate(['/review-info']).then();
     }
+  }
+
+  invalidInput(control: FormControl): boolean {
+    return control.invalid && (control.touched || control.dirty);
   }
 }
