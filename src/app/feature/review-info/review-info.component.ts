6adef1b8af6dcbc61d37dfa51cc7894a858c85cd
Fahimeh Ahmadi <fahimehahmadi.fafa@gmail.com>
2024-07-02 18:11:53 +0330

    show Toaster

--- src/app/feature/review-info/review-info.component.ts
+++ src/app/feature/review-info/review-info.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {UserDataService} from "../../core/services/user-data/user-data.service";
+import {InitialUser, User} from "../../core/models/user-data.model";
 
 @Component({
   selector: 'app-review-info',
@@ -8,7 +9,8 @@ import {UserDataService} from "../../core/services/user-data/user-data.service";
   styleUrl: './review-info.component.css'
 })
 export class ReviewInfoComponent implements OnInit {
-  userData: any;
+  userData: User = InitialUser;
+  isSuccessForm: boolean = false
 
   constructor(private userDataService: UserDataService,
               private router: Router) {
@@ -19,12 +21,29 @@ export class ReviewInfoComponent implements OnInit {
   }
 
   onSubmit() {
-    if (this.userData.personalInfo.firstName && this.userData.personalInfo.lastName && this.userData.personalInfo.nationalCode &&
-      this.userData.contactInfo.province && this.userData.contactInfo.city && this.userData.contactInfo.address &&
+    if (this.userData.personalInfo.firstName &&
+      this.userData.personalInfo.lastName &&
+      this.userData.personalInfo.nationalCode &&
+      this.userData.contactInfo.province &&
+      this.userData.contactInfo.city &&
+      this.userData.contactInfo.address &&
       this.userData.document) {
-      alert('Information submitted successfully!');
-      this.userDataService.clearData();
-      this.router.navigate(['/personal-info']).then();
+      this.isSuccessForm =  !this.isSuccessForm;
+      setTimeout(() => {
+        this.userDataService.clearData();
+        this.router.navigate(['/personal-info']).then();
+      }, 2000)
     }
+  }
+
+  isCheckValidForm(): boolean {
+    return (!this.userData.personalInfo.firstName ||
+      !this.userData.personalInfo.lastName ||
+      !this.userData.personalInfo.nationalCode ||
+      !this.userData.contactInfo.province ||
+      !this.userData.contactInfo.city ||
+      !this.userData.contactInfo.address ||
+      !this.userData.document
+    )
   }
 }
